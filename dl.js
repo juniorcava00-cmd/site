f4c04a0c5e6fb6e2a5d766b2b030b84dfd402f05
juniorcava00-cmd <juniorcava00@gmail.com>
2026-09-18 00:29:30 -0300

    Update Titan Turbo download to V1809 ZIP

--- dl.js
+++ dl.js
@@ -1,4 +1,4 @@
-window.TITAN_SETUP = "https://github.com/juniorcava00-cmd/site/releases/download/v1.0/TitanTurboPRO-Setup.7z";
+window.TITAN_SETUP = "https://github.com/juniorcava00-cmd/site/releases/download/v1.0/TitanTurboPRO-V1809.zip";
 document.addEventListener("click", (e) => {
   const a = e.target.closest("[data-setup]");
   if (!a) return;
