f6c60d2e580ad8f494184e48119fea97b2832ae0
juniorcava00-cmd <juniorcava00@gmail.com>
2026-09-18 21:45:48 -0300

    Add privacy-aware marketing tracking configuration

---
+++ tracking-config.js
@@ -0,0 +1,9 @@
+// IDs públicos de mensuração. Não coloque tokens, senhas ou segredos neste arquivo.
+// Preencha os IDs quando as respectivas contas estiverem criadas.
+// Os pixels só são carregados depois do consentimento do visitante.
+window.TITAN_TRACKING = {
+  ga4MeasurementId: "",
+  metaPixelId: "",
+  tiktokPixelId: "",
+  consentStorageKey: "titan_tracking_consent_v1"
+};
